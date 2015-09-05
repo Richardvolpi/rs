61e6068da544bb2913a07a6441cb60054b728d8d
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 23:01:50 +0100

    Extract ./eXemplar's-collection/exemplar/rs1/rs1/v194/jclient194.zip

---
+++ eXemplar's-collection/exemplar/rs1/rs1/v194/jclient194/Jclient 194/F2P/F2P_SUN.bat
@@ -0,0 +1,25 @@
+@echo off
+echo -------------------------------------------------------------------------------
+echo THIS IS FOR FREE PLAYING PEOPLE (F2P)
+echo THIS BAT FILE IS FOR SUN JAVA IS YOU HAVE PROBLEMS PLEASE USE RUN_MS.
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo MAKE SURE YOU READ THE README FILE BEFORE USE
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo -------------------------------------------------------------------------------
+echo **Thats all just press anykey and have fun**
+echo -------------------------------------------------------------------------------
+pause
+cd..
+echo Please make sure you read this file and also make sure your bug hasent already been reported.
+readme.txt
+pause
+cd filez
+cd switcher
+cd f2p
+START worldswitcher.exe
+echo WorldSwitcher is now loaded please choose your world from the list above.
+cd..
+cd..
+cd class files
+appletviewer.exe -J-noverify runescape.html
+pause
---
+++ eXemplar's-collection/exemplar/rs1/rs1/v194/jclient194/Jclient 194/P2P/Fast P2P_MS.bat
@@ -0,0 +1,22 @@
+@echo off
+echo -------------------------------------------------------------------------------
+echo *******THIS IS FOR MEMBERS ONLY (P2P)*********
+echo THIS BAT FILE IS FOR SUN JAVA IF YOU HAVE PROBLEMS PLEASE USE P2P_MS.
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo MAKE SURE YOU READ THE README FILE BEFORE USE
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo -------------------------------------------------------------------------------
+echo **Thats all just press anykey and have fun**
+echo -------------------------------------------------------------------------------
+cd ..
+cd filez
+cd switcher
+cd P2P
+START P2P`worldswitcher.exe
+echo WorldSwitcher is now loaded please choose your world from the list above.
+cd..
+cd..
+cd class files
+REM For MSJava
+jview /a P2P`runescape.html
+pause
