a369bdfc27ecd6ee2bf882b221c842ad53a41434
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:35 +0100

    Extract ./rs1/rs1/v201/STS201N.rar

---
+++ rs1/rs1/v201/STS201N/Scripts/NewLogin.java
@@ -0,0 +1,11 @@
+public class NewLogin extends Methods
+{
+
+    public NewLogin(mudclient mudclient){super(mudclient);}
+
+    public void run() // Called everytime we login
+    {
+        if(!Running())
+            SetMode(1); // Set us to fightmode 1
+    }
+}
