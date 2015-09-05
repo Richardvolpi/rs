4cdbf86b41618459367331e4dca8aa595c3f095d
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:40 +0100

    Extract ./rs1/rs1/Unsorted/Akbot.rar

---
+++ rs1/rs1/Unsorted/Akbot/Akbotsource/Akbotsource/gnm.java
@@ -0,0 +1,54 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: packimports(3) 
+
+
+final class gnm
+{
+
+    gnm()
+    {
+        hal = new int[10];
+        ham = new int[10];
+        han = new int[12];
+        hbi = -1;
+        hcd = false;
+        hce = -1;
+    }
+
+    public long gnn;
+    public String haa;
+    public int hab;
+    public int hac;
+    public int had;
+    public int hae;
+    public int haf;
+    public int hag;
+    public int hah;
+    public int hai;
+    public int haj;
+    public int hak;
+    public int hal[];
+    public int ham[];
+    public int han[];
+    public String hba;
+    public int hbb;
+    public int hbc;
+    public int hbd;
+    public int hbe;
+    public int hbf;
+    public int hbg;
+    public int hbh;
+    public int hbi;
+    public int hbj;
+    public int hbk;
+    public int hbl;
+    public int hbm;
+    public int hbn;
+    public int hca;
+    public int hcb;
+    public int hcc;
+    public boolean hcd;
+    public int hce;
+    public int hcf;
+}
