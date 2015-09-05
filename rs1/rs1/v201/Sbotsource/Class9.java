59688e370460e0b4e1dec9567d44f09cd853b3b1
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:36 +0100

    Extract ./rs1/rs1/v201/Sbotsource.zip

---
+++ rs1/rs1/v201/Sbotsource/Class9.java
@@ -0,0 +1,17 @@
+// Decompiled by Jad v1.5.8e2. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://kpdus.tripod.com/jad.html
+// Decompiler options: packimports(3) 
+
+
+public class Class9
+{
+
+    public Class9()
+    {
+    }
+
+    int anInt380;
+    int anInt381;
+    int anInt382;
+    int anInt383;
+}
