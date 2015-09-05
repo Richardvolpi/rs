2184aedc0cb349c1accbeaf9ab67e96eb3a8f412
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:51 +0100

    Extract ./rs1/rs1/v193/JC193BySbm007.zip

---
+++ rs1/rs1/v193/JC193BySbm007/JC 193/BotF2P.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+REM JC 193 by Sbm007
+
+REM Use this for microsoft's apppletviewer:
+jview /a botf2p.html
+
+REM Use this for sun's appletviewer
+REM C:\j2sdk1.4.1_02\bin\appletviewer.exe botf2p.html
