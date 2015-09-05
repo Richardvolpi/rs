4128a1eb1707a2167beddd13f7d88566861d0366
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 23:01:41 +0100

    Extract ./eXemplar's-collection/exemplar/rs1/rs1/Unsorted/TRuneXP.rar

---
+++ eXemplar's-collection/exemplar/rs1/rs1/Unsorted/TRuneXP/TRuneXP/TRuneXP/Files/f.java
@@ -0,0 +1,57 @@
+/* deobfuscatorzed by saevion
+ * 
+ * <3 to:
+ *  jakarta-BCEL    <http://jakarta.apache.org/bcel/index.html>
+ *  jakarta-regexp  <http://jakarta.apache.org/regexp/index.html>
+ *  jad             <http://kpdus.tripod.com/jad.html>
+ */
+
+final class f
+{
+
+    f()
+    {
+        gnb = new int[10];
+        gnc = new int[10];
+        gnd = new int[12];
+        gnm = -1;
+        hah = false;
+        hai = -1;
+    }
+
+    public long gmd;
+    public String gme;
+    public int gmf;
+    public int gmg;
+    public int gmh;
+    public int gmi;
+    public int gmj;
+    public int gmk;
+    public int gml;
+    public int gmm;
+    public int gmn;
+    public int gna;
+    public int gnb[];
+    public int gnc[];
+    public int gnd[];
+    public String gne;
+    public int gnf;
+    public int gng;
+    public int gnh;
+    public int gni;
+    public int gnj;
+    public int gnk;
+    public int gnl;
+    public int gnm;
+    public int gnn;
+    public int haa;
+    public int hab;
+    public int hac;
+    public int had;
+    public int hae;
+    public int haf;
+    public int hag;
+    public boolean hah;
+    public int hai;
+    public int haj;
+}
---
+++ eXemplar's-collection/exemplar/rs1/rs1/Unsorted/TRuneXP/TRuneXP/TRuneXP/ReadMe.txt
@@ -0,0 +1,16 @@
+You In NO WAY may:
+1. Rehost
+2. Redistribute
+3. ReCreate
+4. Sell
+5. release link
+6. Remove any copyright
+
+This Was Created By RSTyrant from rstyrant.com
+
+//Shoutouts
+On3 and Dameyez for Inspiring me to Make Scripting RR Style
+Exhaust - Cheating Friend for life
+Reines -You always said i couldnt do it, well, i did so HA
+Antiyou - Encouraged me to Keep Going On
+RegeX - All Most Renamed it RegexRune
