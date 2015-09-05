39ec7365ba05bda189fdf66825002af1d5876772
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 23:01:53 +0100

    Extract ./eXemplar's-collection/exemplar/rs1/rs1/v194/leet_dupe.rar

---
+++ eXemplar's-collection/exemplar/rs1/rs1/v194/leet_dupe/runescape.bat
@@ -0,0 +1,19 @@
+@echo off
+echo -----------------
+echo LEET DUPE BY PR4Y
+echo -----------------
+echo Props to:
+echo eP = compiled for me
+echo Faketrix = RuneHex (leeted)
+echo Random Hero = The idea
+echo ICY = negros
+echo RichyT = My secret l0v0r
+echo -----------------
+echo Press f2 right before
+echo a deadly hit during combat
+echo duels / wild... both work
+echo -----------------
+echo "<3 PR4Y"
+echo -----------------
+java -classpath RuneHex.jar;. AppletViewer http://uk1.runescape.com/client2/ 1 true
+pause
