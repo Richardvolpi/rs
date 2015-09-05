e4763448bad1b23ac5dbc9169739be4e025fa355
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:45 +0100

    Extract ./rs1/rs1/v194/RuneMonkey194.zip

---
+++ rs1/rs1/v194/RuneMonkey194/run_MS.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ----------------------------------
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo MAKE SURE YOU READ THE README FILE BEFORE USE
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo **Thats is all just have fun**
+echo ----------------------------------
+REM For MSJava
+jview /a runescape.html
---
+++ rs1/rs1/v194/RuneMonkey194/run_SUN.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ----------------------------------
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo MAKE SURE YOU READ THE README FILE BEFORE USE
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo **Thats is all just have fun**
+echo ----------------------------------
+appletviewer.exe -J-noverify runescape.html
+rem java -classpath runemonkey.jar AppletViewer http://uk1.runescape.com/client2/ 31
+pause
