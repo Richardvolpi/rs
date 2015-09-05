2587117ce1a9ca46c7908b17fc8f7da39689abef
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:33 +0100

    Extract ./rs1/rs1/v201/client201.zip

---
+++ rs1/rs1/v201/client201/client201/client201/README.txt
@@ -0,0 +1,9 @@
+mudclient201 deobfuscated by saevion, you know the drill, go code some bots.
+
+deobed BY SAEVION
+
+deobed BY SAEVION
+
+-downloaded from www.succinctrsc.com ... I hope, or your pwnt :P
+-dont re-upload pl0z
+
---
+++ rs1/rs1/v201/client201/client201/client201/l.java
@@ -0,0 +1,35 @@
+/* deobfuscatorzed by saevion
+ * 
+ * <3 to:
+ *  jakarta-BCEL    <http://jakarta.apache.org/bcel/index.html>
+ *  jakarta-regexp  <http://jakarta.apache.org/regexp/index.html>
+ *  jad             <http://kpdus.tripod.com/jad.html>
+ */
+
+public class l
+{
+
+    public l()
+    {
+        bld = false;
+        blf = -1;
+    }
+
+    protected int bkd;
+    protected int bke;
+    protected int bkf;
+    protected int bkg;
+    protected int bkh;
+    protected int bki;
+    protected i bkj;
+    protected int bkk;
+    protected int bkl;
+    protected int bkm;
+    protected int bkn;
+    protected int bla;
+    protected int blb;
+    protected int blc;
+    protected boolean bld;
+    protected int ble;
+    protected int blf;
+}
