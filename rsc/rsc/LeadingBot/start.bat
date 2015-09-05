51a09fcf057879beef72f5f6c1b4fcbcbcb055eb
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 23:00:27 +0100

    Extract ./rsc/rsc/LeadingBot.rar

---
+++ rsc/rsc/LeadingBot/start.bat
@@ -0,0 +1,2 @@
+@java mudclient members uk1.runescape.com 43594
+@pause
---
+++ rsc/rsc/LeadingBot/w.java
@@ -0,0 +1,23 @@
+/* deobfuscatorzed by saevion
+ * 
+ * <3 to:
+ *  jakarta-BCEL    <http://jakarta.apache.org/bcel/index.html>
+ *  jakarta-regexp  <http://jakarta.apache.org/regexp/index.html>
+ *  jad             <http://kpdus.tripod.com/jad.html>
+ */
+
+public class w
+{
+
+    public static int cel = 202;
+    public static int cem = 202;
+    public static int cen = 85;
+    public static int cfa = 63;
+    public static int cfb = 58;
+    public static int cfc = 36;
+    public static int cfd = 17;
+    public static int cfe = 24;
+    public static int cff = 1;
+    public static int cfg = 2;
+
+}
