24ba4998b71a549862c7ff37b7608358849dc448
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:33 +0100

    Extract ./rs1/rs1/v201/st.rar

---
+++ rs1/rs1/v201/st/Run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Updated without using mudclient199 - It works, but may be more detectable than before? Basically if your banned don't blame me.
+JAVA -classpath Skull.jar; mudclient
+pause
