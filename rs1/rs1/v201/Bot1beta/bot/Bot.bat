73dfb4332db80629ce347c7a1dc18804b6ce7200
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:35 +0100

    Extract ./rs1/rs1/v201/Bot1beta.rar

---
+++ rs1/rs1/v201/Bot1beta/bot/Bot.bat
@@ -0,0 +1,7 @@
+@echo off
+@echo - - - - - - - - - - - - - - - - - - - -
+@echo  B 	-	O	-	T
+@echo		                201
+@echo - - - - - - - - - - - - - - - - - - - -
+cd Files
+JAVA -cp .\ mudclient
