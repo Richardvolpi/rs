a733087ecaf966b96f029818dc3083b09ebcd819
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-05 22:59:48 +0100

    Extract ./rs1/rs1/v194/jclient194.zip

---
+++ rs1/rs1/v194/jclient194/Jclient 194/F2P/Fast_MS.bat
@@ -0,0 +1,21 @@
+@echo off
+echo -------------------------------------------------------------------------------
+echo THIS IS FOR FREE PLAYING PEOPLE (F2P)
+echo Fast_Ms removes shit from the Run_ms bat file such as pauses
+echo THIS BAT FILE IS FOR MS JAVA IS YOU HAVE PROBLEMS PLEASE USE RUN_SUN.
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo MAKE SURE YOU READ THE README FILE BEFORE USE
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo -------------------------------------------------------------------------------
+cd..
+cd filez
+cd switcher
+cd f2p
+START worldswitcher.exe
+echo WorldSwitcher is now loaded please choose your world from the list above.
+cd..
+cd..
+cd class files
+REM For MSJava
+jview /a RuneScape.html
+pause
---
+++ rs1/rs1/v194/jclient194/Jclient 194/Setup.bat
@@ -0,0 +1,28 @@
+@echo off
+echo ----------------------------------
+echo THIS FILE IS FROM JIMMY'S CLIENT
+echo USE THIS FILE ONLY AS A LAST RESORT IF NOTHING ELSE WORKS.
+echo IF YOU NEED ANY MORE HELP YOU CAN FIND ME ON RSCHEATNET FORUMS
+echo If you wish to continue just press anykey to start getting files.
+echo ----------------------------------
+pause
+cd filez
+cd other
+REM normal stuff
+wget http://209.120.137.228/client2/jagex.jag
+wget http://209.120.137.228/client2/config85.jag
+wget http://209.120.137.228/client2/maps63.jag
+wget http://209.120.137.228/client2/land63.jag
+wget http://209.120.137.228/client2/media58.jag
+wget http://209.120.137.228/client2/models36.jag
+wget http://209.120.137.228/client2/textures17.jag
+wget http://209.120.137.228/client2/entity24.jag
+wget http://209.120.137.228/client2/filter2.jag
+REM member stuff
+wget http://209.120.137.228/client2/maps63.mem
+wget http://209.120.137.228/client2/land63.mem
+wget http://209.120.137.228/client2/entity24.mem
+wget http://209.120.137.228/client2/sounds1.mem
+echo All files recived sucefully.
+pause
+
